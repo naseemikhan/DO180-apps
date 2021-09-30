386aba8221de797ff77f4cd67f4e48c1d9708932
Student User <student@workstation.lab.example.com>
2021-09-30 16:42:48 -0400

    Fixed module typo

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -1,13 +1,13 @@
-var createError = require('http-error');
+var createError = require('html-errors');
 
 var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.environment.HOSTNAME + '\n')
+  res.send('Hello World from Naseems pod: ' + process.environment.HOSTNAME + '\n')
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example Node NIK app listening on port 8080!');
 });
 
